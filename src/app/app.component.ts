0ae5d116279849aa1ae6b26a5bed7fc0a6f450b3
Patrick Navarro <patricknv230101@gmail.com>
2023-01-02 14:21:15 -0500

    Example of Trusted Types with Angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'securityangular-trustedtypes';
+  htmlSnippet = 'Template <script>alert("Your website is hacking")</script> <b>XD</b>';
+  dangerousURL: string;
+  trustedURL: SafeUrl;
+  dangerousVideoUrl: string;
+  videoUrl: any;
+  inputTask: any;
+  inputTrustedTask: any;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.dangerousURL = 'javascript:alert("Hi Mr Robot")';
+    this.trustedURL = sanitizer.bypassSecurityTrustUrl(this.dangerousURL);
+    this.dangerousVideoUrl = '';
+    this.updateVideoUrl('RxssCrRgPHI');
+  }
+
+  updateVideoUrl(id: string) {
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+  }
+
+  addToDom() {
+    this.inputTrustedTask = this.sanitizer.bypassSecurityTrustHtml(this.inputTask);
+  }
+
 }
